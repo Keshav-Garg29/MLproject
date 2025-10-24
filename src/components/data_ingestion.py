b91fe33a5fd1b666f6d0e3941370688af9357ff3
Keshav-Garg29 <keshugarg292005@gmail.com>
2025-10-24 15:37:00 +0530

    Data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass #@dataclass turns the class into a simple data holder with minimal code. @dataclass, Python automatically creates the __init__()
 class DataIngestionConfig: #This class stores all the file paths needed for data ingestion
@@ -43,4 +45,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj= DataIngestion()
-    obj.Initiate_data_ingestion()
+    train_data , test_data = obj.Initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data , test_data)
